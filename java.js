c6fea1c228ce10463b72d8d0d473586176ba4055
Guillermo Heriberto López Pérez <86587084+GHLP123@users.noreply.github.com>
2021-09-06 11:41:11 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function () {
 	console.log('Pagina cargada!!! Espera... ¡¿Que haces en la consola?!');
 
-	document.imagenes1.src = 'img/nodisponible.png'
-	document.imagenes2.src = 'img/nodisponible.png'
-	document.imagenes3.src = 'img/nodisponible.png'
-	document.imagenes4.src = 'img/nodisponible.png'
-	document.imagenes5.src = 'img/nodisponible.png'
-	document.imagenes6.src = 'img/nodisponible.png'
+	document.imagenes1.src = 'img/nodisponible.png';
+	document.imagenes2.src = 'img/nodisponible.png';
+	document.imagenes3.src = 'img/nodisponible.png';
+	document.imagenes4.src = 'img/nodisponible.png';
+	document.imagenes5.src = 'img/nodisponible.png';
+	document.imagenes6.src = 'img/nodisponible.png';
 });
